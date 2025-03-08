639194a918ee9d5730c919c1c548c819ce1cfc67
Patryk5173 <dotey83744@chainds.com>
2025-03-08 15:00:11 +0100

    To jest moj kreator postaci

---
+++ main.py
@@ -0,0 +1,59 @@
+import pygame
+import element
+SZEROKOSC_EKRANU = 800
+WYSOKOSC_EKRANU = 600
+
+obraz_tla = pygame.image.load('images/background.png')
+obraz_bazy_postaci =  pygame.image.load('images/base.png')
+
+pygame.init()
+
+ekran = pygame.display.set_mode([SZEROKOSC_EKRANU, WYSOKOSC_EKRANU])
+zegar = pygame.time.Clock()
+
+moja_czcionka = pygame.font.SysFont('Comic Sans MS', 30)
+
+def wypisz_tekst(ekran, tekst, pozycja):
+    napis = moja_czcionka.render(tekst, False, (255, 255, 255))
+    ekran.blit(napis, pozycja)
+
+nakrycie_glowy = element.NakrycieGlowy()
+ubranie_element = element.UbranieElement()
+oczy_element = element.OczyElement()
+bron_element = element.BronElement()
+
+gra_dziala = True
+while gra_dziala:
+    for zdarzenie in pygame.event.get():
+        if zdarzenie.type == pygame.KEYDOWN:
+            if zdarzenie.key == pygame.K_ESCAPE:
+                gra_dziala = False
+            if zdarzenie.key == pygame.K_q:
+                nakrycie_glowy.wybierzNastepny()
+            if zdarzenie.key == pygame.K_w:
+                oczy_element.wybierzNastepny()
+            if zdarzenie.key == pygame.K_e:
+                ubranie_element.wybierzNastepny()
+            if zdarzenie.key == pygame.K_r:
+                bron_element.wybierzNastepny()
+        elif zdarzenie.type == pygame.QUIT:
+            gra_dziala = False
+
+    ekran.blit(obraz_tla, (0,0))
+    ekran.blit(obraz_bazy_postaci, (270,130))
+    ekran.blit(nakrycie_glowy.wybranyObraz(), (270, 130))
+    ekran.blit(ubranie_element.wybranyObraz(), (270, 130))
+    ekran.blit(oczy_element.wybranyObraz(), (270, 130))
+    ekran.blit(bron_element.wybranyObraz(), (270, 130))
+
+    wypisz_tekst(ekran, f'[Q] Glowa{nakrycie_glowy.wybrany}', (100, 100))
+    wypisz_tekst(ekran, f'[W] Oczy{oczy_element.wybrany}', (100, 140))
+    wypisz_tekst(ekran, f'[E] Ubranie{ubranie_element.wybrany}', (100, 180))
+    wypisz_tekst(ekran, f'[R] Bron{bron_element.wybrany}', (100, 220))
+    pygame.display.flip()
+    zegar.tick(30)
+
+pygame.quit()
+
+
+
